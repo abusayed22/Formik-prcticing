dec2dfcc66f03e1bcecd03733c2648b6e38d5eba
Abu Sayed <abusayed.in22@gmail.com>
2023-03-07 10:39:35 +0600

    form handling from formik state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
       <h2 style={{textAlign:'center'}}>Wellcome Formik </h2>
+      <Register />
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -8,13 +8,29 @@ function Register() {
             email: '',
             password: ''
         },
-
+        onSubmit: ( value ) => {
+            console.log(value);
+        }
     })
 
-    return(<div style={{width: '300px',height: "70vh",}}>
-        <form>
-            <label>Name:</label>
-            
+    return (<div style={{ width: '300px', height: "70vh", }}>
+        <form onSubmit={formik.handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type={'text'} id='name' name="name" value={formik.values.name} onChange={formik.handleChange}/>
+            </div>
+
+            <div>
+                <label>Email:</label>
+                <input type={'email'} id='email' name="email" value={formik.values.email} onChange={formik.handleChange} />
+            </div>
+
+            <div>
+                <label>password:</label>
+                <input type={'password'} id='password' name="password" value={formik.values.password} onChange={formik.handleChange}/>
+            </div>
+
+            <button type="submit">Submit</button>
         </form>
     </div>)
 }
